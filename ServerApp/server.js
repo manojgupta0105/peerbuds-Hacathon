59a4a89050860987ce849474d2686bf467477fc6
Manoj S Gupta <manojgupta0105@gmail.com>
2016-12-11 17:10:28 +0530

    Server code added

--- ServerApp/server.js
+++ ServerApp/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
 // ====================================
 // URL PARAMETERS =====================
 // ====================================
-// http://localhost:8080/api/users?id=4&token=sadsf4&geo=us
+// http://localhost:8080/getJSONData?filename=
 
 app.get('/getJSONData', function(req, res, next) {
   	var filename = req.param('filename');
@@ -47,44 +47,6 @@ app.get('/getJSONData', function(req, res, next) {
 
 });
 
-// http://localhost:8080/api/1
-app.get('/api/:version', function(req, res) {
-	res.send(req.params.version);
-});
-
-// parameter middleware that will run before the next routes
-app.param('name', function(req, res, next, name) {
-
-	// check if the user with that name exists
-	// do some validations
-	// add -dude to the name
-	var modified = name + '-dude';
-
-	// save name to the request
-	req.name = modified;
-
-	next();
-});
-
-// http://localhost:8080/api/users/chris
-app.get('/api/users/:name', function(req, res) {
-	// the user was found and is available in req.user
-	res.send('What is up ' + req.name + '!');
-});
-
-// ====================================
-// POST PARAMETERS ====================
-// ====================================
-
-// POST http://localhost:8080/api/users
-// parameters sent with 
-app.post('/api/users', function(req, res) {
-	var user_id = req.body.id;
-	var token = req.body.token;
-	var geo = req.body.geo;
-
-	res.send(user_id + ' ' + token + ' ' + geo);
-});
 
 // start the server
 app.listen(port);
